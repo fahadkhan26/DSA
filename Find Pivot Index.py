b70bb482efdc73adf9daa414e2d4650a57e0cd50
Fahad Khan <fahadkhan11899@gmail.com>
2025-01-05 22:43:38 +0500

    leetcode

---
+++ Find Pivot Index.py
@@ -0,0 +1,13 @@
+def pivotIndex(nums):
+    total_sum = sum(nums)
+    leftSum = 0
+    rightSum = 0
+
+    for i in range(len(nums)):
+        rightSum = total_sum - leftSum - nums[i]
+        if leftSum == rightSum:
+            return i
+        else:
+            leftSum += nums[i]
+            
+    return -1
