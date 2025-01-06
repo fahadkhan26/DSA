e6598c143c6c70816e067d596e1e75d9dde95396
Fahad Khan <fahadkhan11899@gmail.com>
2025-01-06 22:34:13 +0500

    Leetcode

---
+++ Move Zeroes.py
@@ -0,0 +1,7 @@
+def moveZeroes(nums):
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] != 0:
+            nums[right], nums[left] = nums[left], nums[right]
+            left += 1
+    return nums
