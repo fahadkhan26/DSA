77bb99bfccb2250ed320026d4946c8ad74b76e08
Fahad Khan <fahadkhan11899@gmail.com>
2025-01-11 23:29:26 +0500

    Leetcode

--- Max Consecutive Ones III.py
+++ Max Consecutive Ones III.py
@@ -7,14 +7,9 @@ def maxConOnes(nums, k):
     for r in range(len(nums)):
         if nums[r] == 0:
             zeros += 1
-
-            while zeros > k:
-                if nums[l] == 0:
-                    zeros -= 1
-                    l += 1
-                else:
-                    l += 1
-
+        while zeros > k:
+            if nums[l] == 0:
+                zeros -= 1
+            l += 1
         maxlen = max(maxlen, r - l + 1)
-
     return maxlen
